1d618380317c1af1a2dde60f2ba9f5462247b8d1
Saaim25 <76088337+Saaim25@users.noreply.github.com>
2023-06-27 00:16:04 +0530

    Update projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -3,21 +3,21 @@ const router = express.Router();
 const projectsController = require("../controllers/projectsController");
 
 // Route to display the list of projects
-router.get("/", projectsController.getProjectList);
+router.get("/projects", projectsController.getProjectList);
 
 // Route to handle creating a new project
-router.post("/", projectsController.createProject);
+router.post("/projects", projectsController.createProject);
 
 // Route to handle deleting a project
-router.delete("/:id", projectsController.deleteProject);
+router.delete("/projects/:id", projectsController.deleteProject);
 
 // Display project details
-router.get("/:id", projectsController.getProjectDetails);
+router.get("/projects/:id", projectsController.getProjectDetails);
 
 //Route to handle creating a new Issue
-router.post("/:id/issues", projectsController.createIssue);
+router.post("/projects/:id/issues", projectsController.createIssue);
 
 // Delete an issue
-router.delete("/:id/issues/:issueId", projectsController.deleteIssue);
+router.delete("/projects/:id/issues/:issueId", projectsController.deleteIssue);
 
 module.exports = router;
