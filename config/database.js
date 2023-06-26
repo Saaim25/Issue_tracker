a85577d69cf0a93b5bbe9c15f3580a90c02e2c44
Saaim25 <76088337+Saaim25@users.noreply.github.com>
2023-06-27 00:38:49 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-const connectDB = (url) => {
-  return mongoose.connect(url);
+const connectDB = async (url) => {
+  try {
+    const conn = await mongoose.connect(url);
+    console.log(`MongoDB connected`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
